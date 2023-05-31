9003cefe9f6d1aeb20173428e6b3f3ffb38aae8a
Jhonattan Mercado <jmercadosenior@gmail.com>
2023-05-31 12:25:16 -0500

    Aula 1 finalizada

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
+
+function App() {
+
+ 
+  return (
+    <>
+      <Router >
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Sobre' element={<Sobre />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
